e9fbdbafa6b73cf9645f5229af8b3f23be53ad67
Paul Irish <paul.irish@gmail.com>
2017-05-15 21:59:52 -0700

    Report: don't display score gauge header if there's only one.

--- lighthouse-core/report/v2/renderer/report-renderer.js
+++ lighthouse-core/report/v2/renderer/report-renderer.js
@@ -139,18 +139,21 @@ class ReportRenderer {
    */
   _renderReport(report) {
     const container = this._dom.createElement('div', 'lh-container');
+    const isSoloCategory = report.reportCategories.length === 1;
 
     container.appendChild(this._renderReportHeader(report)); // sticky header goes at the top.
     container.appendChild(this._renderReportNav(report));
 
     const reportSection = container.appendChild(this._dom.createElement('div', 'lh-report'));
 
-    const scoreHeader = reportSection.appendChild(
-        this._dom.createElement('div', 'lh-scores-header'));
+    const scoreHeaderClass = `lh-scores-header ${isSoloCategory ? 'lh-scores-header__solo' : ''}`;
+    const scoreHeader = reportSection.appendChild(this._dom.createElement('div', scoreHeaderClass));
 
     const categories = reportSection.appendChild(this._dom.createElement('div', 'lh-categories'));
+
     for (const category of report.reportCategories) {
-      scoreHeader.appendChild(this._categoryRenderer.renderScoreGauge(category));
+      if (!isSoloCategory)
+        scoreHeader.appendChild(this._categoryRenderer.renderScoreGauge(category));
       categories.appendChild(this._categoryRenderer.render(category, report.reportGroups));
     }
 
