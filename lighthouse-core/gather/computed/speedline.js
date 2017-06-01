8b281df2e66600c036049353d6851a8ed9537aee
Brendan Kenny <bckenny@google.com>
2017-06-01 14:54:08 -0700

    isolate speedline's trace sorting from LH

--- lighthouse-core/gather/computed/speedline.js
+++ lighthouse-core/gather/computed/speedline.js
@@ -32,10 +32,13 @@ class Speedline extends ComputedArtifact {
     // speedline() may throw without a promise, so we resolve immediately
     // to get in a promise chain.
     return computedArtifacts.requestTraceOfTab(trace).then(traceOfTab => {
+      // Use a shallow copy of traceEvents so speedline can sort as it pleases.
+      // See https://github.com/GoogleChrome/lighthouse/issues/2333
+      const traceEvents = trace.traceEvents.slice();
       // Force use of nav start as reference point for speedline
       // See https://github.com/GoogleChrome/lighthouse/issues/2095
       const navStart = traceOfTab.timestamps.navigationStart * 1000;
-      return speedline(trace.traceEvents, {
+      return speedline(traceEvents, {
         timeOrigin: navStart,
         fastMode: true,
       });
